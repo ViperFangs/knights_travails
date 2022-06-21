a51fb87228d705a216a499d8b394fae2f418bf80
Aarya <amathrej@asu.edu>
2022-06-20 22:02:22 -0700

    Implement Knight class and #possible_moves

---
+++ lib/knight.rb
@@ -0,0 +1,52 @@
+class Knight
+  attr_accessor :position, :next_moves
+
+  def initialize(position_array)
+    self.position = position_array
+    self.next_moves = possible_moves(position_array)
+  end
+
+  def possible_moves(position_array)
+    board_end_point = 8 
+    board_start_point = 0 
+    return_moves = []
+    current_row = position_array[0]
+    current_column = position_array[1]
+
+    if current_row + 2 < board_end_point && current_column + 1 < board_end_point
+      return_moves.push([current_row + 2, current_column + 1])
+    end
+
+    if current_row + 2 < board_end_point && current_column - 1 >= board_start_point
+      return_moves.push([current_row + 2, current_column - 1])
+    end
+
+    if current_row - 2 >= board_start_point && current_column + 1 < board_end_point
+      return_moves.push([current_row - 2, current_column + 1])
+    end
+
+    if current_row - 2 >= board_start_point && current_column - 1 >= board_start_point
+      return_moves.push([current_row - 2, current_column - 1])
+    end
+
+    if current_row + 1 < board_end_point && current_column + 2 < board_end_point
+      return_moves.push([current_row + 1, current_column + 2])
+    end
+
+    if current_row - 1 >= board_start_point && current_column + 2 < board_end_point
+      return_moves.push([current_row - 1, current_column + 2])
+    end
+
+    if current_row + 1 < board_end_point && current_column - 2 >= board_start_point
+      return_moves.push([current_row + 1, current_column - 2])
+    end
+
+    if current_row - 1 >= board_start_point && current_column - 2 >= board_start_point
+      return_moves.push([current_row - 1, current_column - 2])
+    end
+
+    return_moves
+  end
+end
+
+p Knight.new([3,3]).next_moves
